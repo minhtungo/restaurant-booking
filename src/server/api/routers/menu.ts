974a60575a3eb0e384075c3dd3f812061594ccc0
Minh Tu Ngo <loducfc@gmail.com>
2023-03-28 15:44:33 -0400

    feat: add getMenuItems route

--- src/server/api/routers/menu.ts
+++ src/server/api/routers/menu.ts
@@ -1,51 +1,20 @@
-import { z } from "zod";
-
-import {
-  adminProcedure,
-  createTRPCRouter,
-  publicProcedure,
-} from "@/server/api/trpc";
-import { SignJWT } from "jose";
-import { nanoid } from "nanoid";
-import { getJwtSecretKey } from "@/lib/auth";
-import cookie from "cookie";
-import { TRPCError } from "@trpc/server";
 import { s3 } from "@/lib/s3";
-import { MAX_FILE_SIZE } from "@/constants/config";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 export const menuRouter = createTRPCRouter({
-  getMenuItems: publicProcedure
-    .input(z.object({ email: z.string().email(), password: z.string() }))
-    .mutation(async ({ ctx, input }) => {
-      const { res } = ctx;
-      const { email, password } = input;
+  getMenuItems: publicProcedure.query(async ({ ctx }) => {
+    const menuItems = await ctx.prisma.menuItem.findMany();
 
-      if (
-        email === process.env.ADMIN_EMAIL &&
-        password === process.env.ADMIN_PASSWORD
-      ) {
-        const token = await new SignJWT({})
-          .setProtectedHeader({ alg: "HS256" })
-          .setJti(nanoid())
-          .setIssuedAt()
-          .setExpirationTime("1h")
-          .sign(new TextEncoder().encode(getJwtSecretKey()));
+    const withUrls = await Promise.all(
+      menuItems.map(async (item) => ({
+        ...item,
+        url: await s3.getSignedUrlPromise("getObject", {
+          Bucket: process.env.AWS_S3_BUCKET_NAME,
+          Key: item.imageKey,
+        }),
+      }))
+    );
 
-        res.setHeader(
-          "Set-Cookie",
-          cookie.serialize("user-token", token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            path: "/",
-          })
-        );
-
-        return { success: true };
-      }
-
-      throw new TRPCError({
-        code: "UNAUTHORIZED",
-        message: "Invalid email or password",
-      });
-    }),
+    return withUrls;
+  }),
 });
