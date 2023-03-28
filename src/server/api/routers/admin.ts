886971cddb723e251c0d15bb1ae6d9e5f24dcdb5
Minh Tu Ngo <loducfc@gmail.com>
2023-03-28 14:38:22 -0400

    feat: add trpc api route for delete and add menu item

--- src/server/api/routers/admin.ts
+++ src/server/api/routers/admin.ts
@@ -10,6 +10,8 @@ import { nanoid } from "nanoid";
 import { getJwtSecretKey } from "@/lib/auth";
 import cookie from "cookie";
 import { TRPCError } from "@trpc/server";
+import { s3 } from "@/lib/s3";
+import { MAX_FILE_SIZE } from "@/constants/config";
 
 export const adminRouter = createTRPCRouter({
   login: publicProcedure
@@ -45,5 +47,74 @@ export const adminRouter = createTRPCRouter({
         code: "UNAUTHORIZED",
         message: "Invalid email or password",
       });
+    }),
+  createPresignedUrl: adminProcedure
+    .input(z.object({ fileType: z.string() }))
+    .mutation(async ({ input }) => {
+      const id = nanoid();
+      const ex = input.fileType.split("/")[1];
+      const key = `${id}.${ex}`;
+
+      const { url, fields } = (await new Promise((resolve, reject) => {
+        s3.createPresignedPost(
+          {
+            Bucket: "restaurant-booking",
+            Fields: { key },
+            Expires: 60,
+            Conditions: [
+              ["content-length-range", 0, MAX_FILE_SIZE],
+              ["starts-with", "$Content-Type", "image/"],
+            ],
+          },
+          (err, signed) => {
+            if (err) return reject(err);
+            resolve(signed);
+          }
+        );
+      })) as any as { url: string; fields: any };
+
+      return { url, fields, key };
+    }),
+  addMenuItem: adminProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        price: z.number(),
+        imageKey: z.string(),
+        categories: z.array(
+          z.union([
+            z.literal("breakfast"),
+            z.literal("lunch"),
+            z.literal("dinner"),
+          ])
+        ),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const { imageKey, name, categories, price } = input;
+      const menuItem = await ctx.prisma.menuItem.create({
+        data: {
+          imageKey,
+          name,
+          categories,
+          price,
+        },
+      });
+
+      return menuItem;
+    }),
+  deleteMenuItem: adminProcedure
+    .input(z.object({ imageKey: z.string(), id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      // Delete image from s3
+      const { imageKey, id } = input;
+      await s3
+        .deleteObject({ Bucket: "restaurant-booking", Key: imageKey })
+        .promise();
+
+      // Delete image from db
+      await ctx.prisma.menuItem.delete({ where: { id } });
+
+      return true;
     }),
 });
