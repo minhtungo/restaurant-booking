f22585fcd9eb701c6b38e57698e2f4dfaa127cbb
Minh Tu Ngo <loducfc@gmail.com>
2023-03-30 22:59:36 -0400

    feat: add getCartItems route

--- src/server/api/routers/menu.ts
+++ src/server/api/routers/menu.ts
@@ -1,5 +1,6 @@
 import { s3 } from "@/lib/s3";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { z } from "zod";
 
 export const menuRouter = createTRPCRouter({
   getMenuItems: publicProcedure.query(async ({ ctx }) => {
@@ -20,4 +21,31 @@ export const menuRouter = createTRPCRouter({
   checkMenuStatus: publicProcedure.query(async () => {
     return { success: true };
   }),
+  getCartItems: publicProcedure
+    .input(z.array(z.object({ id: z.string(), quantity: z.number() })))
+    .query(async ({ ctx, input }) => {
+      const menuItems = await ctx.prisma.menuItem.findMany({
+        where: {
+          id: {
+            in: input.map((item) => item.id),
+          },
+        },
+      });
+
+      // Each menu items only contains its AWS key. Extend all items with their actual img url
+      const withUrls = await Promise.all(
+        menuItems.map(async (menuItem) => {
+          return {
+            ...menuItem,
+            url: await s3.getSignedUrlPromise("getObject", {
+              Bucket: "restaurant-booking",
+              Key: menuItem.imageKey,
+            }),
+            quantity: input.find((item) => item.id === menuItem.id)?.quantity,
+          };
+        })
+      );
+
+      return withUrls;
+    }),
 });
